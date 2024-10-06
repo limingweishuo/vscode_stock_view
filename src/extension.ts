4df382e422f27e0421365a64828d04f4b467eb73
“ming.li” <ming.li@autodesk.com>
2024-10-06 22:14:24 +0800

    主侧边栏添加treeview

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,16 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Extension is now active!');
 
+	// 定义不同视图的数据源
+	const helloworldView = vscode.window.createTreeView('helloworld-view', { treeDataProvider: new MyTreeDataProvider('Hello World Content') });
+	const view1 = vscode.window.createTreeView('view1', { treeDataProvider: new MyTreeDataProvider('Content for View 1') });
+	// const view2 = vscode.window.createTreeView('view2', { treeDataProvider: new MyTreeDataProvider('Content for View 2') });
+	// const view3 = vscode.window.createTreeView('view3', { treeDataProvider: new MyTreeDataProvider('Content for View 3') });
+
+	// 将这些视图添加到 context 中
+	// context.subscriptions.push(helloworldView, view1, view2, view3);
+	context.subscriptions.push(helloworldView, view1);
+
     // 注册 command1，绑定到 featureOne
     const command1 = vscode.commands.registerCommand('extension.command1', () => {
         featureOne();
@@ -15,6 +25,26 @@ export function activate(context: vscode.ExtensionContext) {
 
     // 将命令加入 context.subscriptions 中，以确保插件被释放时注销
     context.subscriptions.push(command1, command2);
+}
+
+// 自定义 TreeDataProvider 用于提供数据
+class MyTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+    private content: string;
+
+    constructor(content: string) {
+        this.content = content;
+    }
+
+    getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
+        return [
+            new vscode.TreeItem(this.content),
+            new vscode.TreeItem('More items here...'),
+        ];
+    }
 }
 
 // 功能一
